192c01fb63daa23f75be7b4a7bf38397aee6ef92
EvgeniyPosohin <mad.house@bk.ru>
2023-06-14 00:11:57 +0500

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import module
+
+
+
+if __name__ == '__main__':
+    phonebook = module.Phonebook()
+    phonebook.get_list()
+    phonebook.get_correction()
+    phonebook.new_contact
+    phonebook.record()
+
+
---
+++ module.py
@@ -0,0 +1,63 @@
+import csv
+import re
+
+
+class Phonebook:
+    def __init__(self):
+        self.contacts_list = None
+        self.new_contact = []
+
+
+# выгрузка из файла
+    def get_list(self) -> list:
+        with open("phonebook_raw.csv") as f:
+            rows = csv.reader(f, delimiter=",")
+            self.contacts_list = list(rows)
+            return self.contacts_list
+
+
+ # запись нового csv
+    def record(self):
+        with open("phonebook.csv", "w") as f:
+            datawriter = csv.writer(f, delimiter=',')
+            datawriter.writerows(self.new_contact)
+
+
+# Корректируем ФИО
+    def correct_name(self, item: list):
+        pattern = re.compile("^(\w+)([\s,]\w+)?([\s,]\w+)")
+        text = " ".join(item)
+        result = pattern.search(text)
+        item[:2] = result.group(0).split()
+        return item
+
+# Поиск по совпадению ФИ
+    def chek_name(self, item: list):
+        for items in self.new_contact:
+            if item[0] == items[0] and item[1] == items[1]:
+                for id, _ in enumerate(items):
+                    if len(str(_)) <= 1:
+                        items[id] = item[id]
+                return
+        else:
+            self.new_contact.append(item)
+            return
+
+
+# Корректируем номер
+    def correct_phone(self, item: list):
+        pattern = re.compile(r"(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*"
+                             r"(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*")
+        subst_pattern = r"+7(\2)\3-\4-\5 \6\7"
+        result = pattern.sub(subst_pattern, item[6])
+        item[6] = result
+        return item
+
+
+# Со
+    def get_correction(self):
+        self.new_contact.append(self.contacts_list[0])
+        for item in self.contacts_list[1:]:
+            self.correct_name(item)
+            self.correct_phone(item)
+            self.chek_name(item)
